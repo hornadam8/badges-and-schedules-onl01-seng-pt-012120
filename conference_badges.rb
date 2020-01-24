8448a6adbc01f0f800f9eacd758967f629c79a01
Adam G Horn <hornadam8@gmail.com>
2020-01-24 01:15:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,3 +21,11 @@ def assign_rooms(array)
   end
   next_array
 end
+
+def printer(array)
+  print_array = []
+  array.each do |speaker|
+    print_array << [assign_rooms(#{speaker}), batch_badge_creator(#{speaker})]
+  end
+  print_array
+end
