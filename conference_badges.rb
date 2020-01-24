216ccbf69da89d424dc47a9684022cb2101f2feb
Adam G Horn <hornadam8@gmail.com>
2020-01-24 00:45:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
+def batch_badge_creator(speaker)
   speakers.each do |speaker|
     "Hello, my name is #{speaker}"
   end
