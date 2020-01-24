e3ce3a7099e3bc0086c07865480d4f6eab893489
Adam G Horn <hornadam8@gmail.com>
2020-01-24 00:43:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,9 @@ speakers = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator
+  speakers.each do |speaker|
+    "Hello, my name is #{speaker}"
+  end
+end
